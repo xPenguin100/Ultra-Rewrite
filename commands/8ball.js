5bc66df6beb909174ed1131f6282d13751cb2b4c
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-09-06 13:42:05 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -8,7 +8,7 @@ module.exports = {
 
     async run (client, message, args) {
         if(!args[0]) return message.reply('Please ask a full question!')
-        let replies = ["yes.", "Outlook seems good.", "yus", "of course.", "Yes – definitely.", "no.", "Better not tell you now.", "Outlook is not so good..", "nah", "never", "Cannot predict now.", "I dont know.", "I dont know *yet*...", "not a chance.", "I think so.", "only for today!", "not for today c:", "sadly yes..", "sadly no..", "maybe!", "ask again.. later.."];
+        let replies = ["yes.", "Outlook seems good.", "of course.", "Yes – definitely.", "no.", "Better not tell you now.", "Outlook is not so good..", "nah", "never", "Cannot predict now.", "I dont know.", "I dont know *yet*...", "not a chance.", "I think so.", "only for today!", "not for today c:", "sadly yes..", "sadly no..", "maybe!", "ask again.. later.."];
         
         let result = Math.floor((Math.random() * replies.length));
         let question = args.slice().join(" ");

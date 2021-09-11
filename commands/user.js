fe42d8d50c4299e68f26d0ae955694cf2ece557c
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-09-11 17:13:53 -0400

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -1,29 +1,28 @@
-const { MessageEmbed, user } = require('discord.js')
+const { MessageEmbed, Discord, user } = require('discord.js')
 
 module.exports = {
     name: 'user',
     description: 'User info command',
     execute(message) {
-  
-  
-      const Discord = require('discord.js')
-      const author = message.mentions.users.first() || message.author
+
+      const user = message.mentions.users.first() 
+	const author = message.mentions.users.first() || message.author
       const member = message.mentions.members.first() || message.member
       let avatar = author.displayAvatarURL({size: 4096, dynamic: true});
   
       let embed = new MessageEmbed()
-      .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
-      .setDescription(`👤 **${message.author.username}**\n**Tag:** ${message.author.tag}\n**ID:** \`${message.author.id}\`\n**Nickname:** ${member.nickname}\n**Badges:** \`${author.flags.toArray()}\`\n**Bot:** ${author.bot}\n**Joined At**\n**Joined Server:** ${member.joinedAt}\n**Joined Discord:** ${author.createdAt}\n**❓ Other**\n**Bot:** ${author.bot}\n**Roles:** ${message.member.roles.cache.size}`)
-      .addFields(
-        { name: 'Name', value: (`${author.username}`), inline: true},
-        { name: 'Nickname', value: (`${member.nickname}`), inline: true},
-        { name: 'ID', value: (`${author.id}`), inline: true},
-        { name: 'Status', value: (`${message.member.presence.status}`), inline: true},
-        { name: 'Server', value: (`${message.guild.name}`), inline: true},
-        { name: 'Bot', value: (`${author.bot}`), inline: true},
-        { name: 'Joined Server', value: (`${member.joinedAt}`)},
-        { name: 'Joined Discord', value: (`${author.createdAt}`)}
-      )
+      .setAuthor(message.author.tag, user.displayAvatarURL({ dynamic: true}))
+      .setDescription(`👤 **${user.username}**\n**Tag:** ${user.tag}\n**ID:** \`${user.id}\`\n**Nickname:** ${member.nickname}\n**Badges:** \`${user.flags.toArray()}\`\n**Joined At**\n**Joined Server:** ${member.joinedAt}\n**Joined Discord:** ${user.createdAt}\n**❓ Other**\n**Bot:** ${user.bot}\n**Roles:** ${GuildMember.roles.toArray()} ${message.member.roles.cache.size}`)
+      //.addFields(
+        //{ name: 'Name', value: (`${author.username}`), inline: true},
+        //{ name: 'Nickname', value: (`${member.nickname}`), inline: true},
+        //{ name: 'ID', value: (`${author.id}`), inline: true},
+        //{ name: 'Status', value: (`${message.member.presence.status}`), inline: true},
+        //{ name: 'Server', value: (`${message.guild.name}`), inline: true},
+        //{ name: 'Bot', value: (`${author.bot}`), inline: true},
+        //{ name: 'Joined Server', value: (`${member.joinedAt}`)},
+        //{ name: 'Joined Discord', value: (`${author.createdAt}`)}
+      //)
   
       .setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
       .setTimestamp()

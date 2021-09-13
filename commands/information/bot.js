8178592454277e1e257631a4f1ecf84b3324ddf1
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-09-12 21:53:36 -0400

    Update bot.js

--- commands/information/bot.js
+++ commands/information/bot.js
@@ -15,11 +15,12 @@ module.exports = {
 
     
         let embed = new MessageEmbed()
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-        .setTitle('Bot Panel')
-        .setImage('https://media.discordapp.net/attachments/872963017741066331/872963055187800124/Ultra-Banner.jpg?width=1179&height=663')
-        .setDescription(`**🏛 Guilds:** ${message.client.guilds.cache.size}\n**👥 Users:** ${message.client.users.cache.size}\n**👤 Owner:** <@515124684946276362>\n**⚙ Version:** 1.5.0\n\n**📈 Stats:**\n**Ping:** ${Date.now() - message.createdTimestamp}ms.\n**Uptime:** ${client.ws.uptime} (60K ms = 1m, 3.6M ms = 1h)`)
+        //.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        //.setTitle('Bot Panel')
+        //.setImage('https://media.discordapp.net/attachments/872963017741066331/872963055187800124/Ultra-Banner.jpg?width=1179&height=663')
+        //.setDescription(`**🏛 Guilds:** ${message.client.guilds.cache.size}\n**👥 Users:** ${message.client.users.cache.size}\n**👤 Owner:** <@515124684946276362>\n**⚙ Version:** 1.5.0\n\n**📈 Stats:**\n**Ping:** ${Date.now() - message.createdTimestamp}ms.\n**Uptime:** ${client.ws.uptime} (60K ms = 1m, 3.6M ms = 1h)`)
         .setColor('#2F3136')
+        .setDescription(`❌ Command is under maintenance.`)
         message.channel.send({ embeds: [embed] });
 
     }

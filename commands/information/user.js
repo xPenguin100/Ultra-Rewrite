33001205ab8662637a2cb4229a97a97feefe0cf3
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-09-19 14:08:16 -0400

    Update user.js

--- commands/information/user.js
+++ commands/information/user.js
@@ -9,26 +9,26 @@ module.exports = {
       let member = message.mentions.members.first() || message.member
       let avatar = message.author.displayAvatarURL({size: 4096, dynamic: true});
 
-      if(!user) return message.reply(`❌ The user either doesn't exist or you never gave a user!`)
+      //if(!user) return message.reply(`❌ The user either doesn't exist or you never gave a user!`)
   
       let embed = new MessageEmbed()
-      .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
-      .setDescription(`👤 **${user.username}**\n**Tag:** ${user.tag}\n**ID:** \`${user.id}\`\n**Nickname:** ${member.nickname}\n**Badges:** \`${user.flags.toArray()}\`\n**Joined At**\n**Joined Server:** ${member.joinedAt}\n**Joined Discord:** ${user.createdAt}\n**❓ Other**\n**Bot:** ${user.bot}\n**Roles:** ${GuildMember.roles.cache.size} ${message.member.roles.cache.size}`)
-      .addFields(
-        { name: 'Name', value: (`${author.username}`), inline: true},
-        { name: 'Nickname', value: (`${member.nickname}`), inline: true},
-        { name: 'ID', value: (`${author.id}`), inline: true},
-        { name: 'Status', value: (`${message.member.presence.status}`), inline: true},
-        { name: 'Server', value: (`${message.guild.name}`), inline: true},
-        { name: 'Bot', value: (`${author.bot}`), inline: true},
-        { name: 'Joined Server', value: (`${member.joinedAt}`)},
-        { name: 'Joined Discord', value: (`${author.createdAt}`)}
-      )
+      //.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
+      //.setDescription(`👤 **${user.username}**\n**Tag:** ${user.tag}\n**ID:** \`${user.id}\`\n**Nickname:** ${member.nickname}\n**Badges:** \`${user.flags.toArray()}\`\n**Joined At**\n**Joined Server:** ${member.joinedAt}\n**Joined Discord:** ${user.createdAt}\n**❓ Other**\n**Bot:** ${user.bot}\n**Roles:** ${GuildMember.roles.cache.size} ${message.member.roles.cache.size}`)
+      //.addFields(
+        //{ name: 'Name', value: (`${author.username}`), inline: true},
+        //{ name: 'Nickname', value: (`${member.nickname}`), inline: true},
+        //{ name: 'ID', value: (`${author.id}`), inline: true},
+       // { name: 'Status', value: (`${message.member.presence.status}`), inline: true},
+       // { name: 'Server', value: (`${message.guild.name}`), inline: true},
+       // { name: 'Bot', value: (`${author.bot}`), inline: true},
+       // { name: 'Joined Server', value: (`${member.joinedAt}`)},
+       // { name: 'Joined Discord', value: (`${author.createdAt}`)}
+      //)
   
-      .setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
-      .setTimestamp()
-      //.setDescription('❌ Command is down for maintenance.')
-      //.setColor('#2F3136')
+      //.setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
+      //.setTimestamp()
+      .setDescription('❌ Command is down for maintenance.')
+      .setColor('#2F3136')
       message.channel.send({ embeds: [embed] });
     }
   }

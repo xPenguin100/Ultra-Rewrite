5f2ff16e2804ecf838c039b3a8682b0084218119
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-09-27 22:45:07 -0400

    Update achieve.js

--- commands/fun/achieve.js
+++ commands/fun/achieve.js
@@ -1,23 +1,17 @@
-const fetch = require('node-fetch')
 const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: 'achieve',
-    description: 'Achieve something!',
+    description: 'Achieve',
 run: async (client, message, args) => {
-
 const number = args[0];
-const text = args.slice(1).join(' ');
+const text = args.slice(1).join(' ').join('+');
 if (!number) return message.reply("Provide a number")
 if (!text) return message.reply("Provide some text")
-fetch(`https://minecraftskinstealer.com/achievement/${number}/Advancement+Made!/${text}`)
-.then((res) => res.json())
-.then((data) => {
 let embed = new MessageEmbed()
 .setTitle("Advancement Made!")
-.setImage(data.message)
+.setImage(`https://minecraftskinstealer.com/achievement/${number}/Advancement+Made!/${text}`)
 .setColor('#2e3137')
 .setTimestamp()
 message.reply({embeds : [embed]})
-        })
     }
 }

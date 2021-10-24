f8cfebb4597a66058939497d98322f7cc83c4937
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-10-24 11:48:01 -0400

    Update channelinfo.js

--- commands/information/channelinfo.js
+++ commands/information/channelinfo.js
@@ -12,7 +12,7 @@ module.exports = {
       const member = message.mentions.members.first() || message.member
  
       let embed = new MessageEmbed()
-      .setDescription(`#️⃣ **${channel.name}**\n**ID:** \`${channel.id}\`\n**Type:** \`${channel.type}\`\n**Created At:** ${channel.createdAt} ${moment().format('MMMM Do YYYY, h:mm:ss a')}\n**Topic:** ${channel.topic !== null ? `${channel.topic}` : 'No topic found'}`)
+      .setDescription(`#️⃣ **${channel.name}**\n**ID:** \`${channel.id}\`\n**Type:** \`${channel.type}\`\n**Created At:** ${channel.createdAt (moment().format('MMMM Do YYYY, h:mm:ss a'))}\n**Topic:** ${channel.topic !== null ? `${channel.topic}` : 'No topic found'}`)
     .setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
     .setTimestamp()
     .setColor('#2F3136')

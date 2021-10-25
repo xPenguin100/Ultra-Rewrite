6e66cd0c2b3e6721fe45753a408675bdb45f9662
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-10-25 16:06:28 -0400

    Update server.js

--- commands/information/server.js
+++ commands/information/server.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, user } = require('discord.js')
+const { MessageEmbed, Discord } = require('discord.js')
 
 module.exports = {
     name: 'server',
@@ -6,30 +6,29 @@ module.exports = {
     execute(message) {
   
   
-      //const Discord = require('discord.js')
-      //const author = message.mentions.users.first() || message.author
-      //const member = message.mentions.members.first() || message.member
-      //let avatar = author.displayAvatarURL({size: 4096, dynamic: true});
+      const author = message.mentions.users.first() || message.author
+      const member = message.mentions.members.first() || message.member
+      let avatar = author.displayAvatarURL({size: 4096, dynamic: true});
   
       let embed = new MessageEmbed()
-    //.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
-    //.setTitle('Server Info')
-    //.setDescription(`Hello ${message.author.tag}, you requested to see the latest info on this server. They are found below!`)
-    //.addFields(
-      //{ name: 'Server\'s Name', value: (`${message.guild.name}`)},
-      //{ name: 'Owner', value: (`${message.guild.owner} (${message.guild.owner.id})`)},
-     // { name: 'Server\'s ID', value: (`${message.guild.id}`)},
-     // { name: 'Region', value: (`${message.guild.region}`)},
-    //  { name: 'Member Count', value: (`${message.guild.memberCount}`)},
-    //  { name: 'Boost Tier', value: (`${message.guild.premiumTier}`)},
-    //  { name: 'Role Count', value: (`${message.guild.roles.cache.size}`)},
-     // { name: 'Emoji Count', value: (`${message.guild.emojis.cache.size}`)},
-     // { name: 'Created On', value: (`${message.guild.createdAt}`)}
-    //)
-    //.setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
-    //.setTimestamp()
+    .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
+    .setTitle('Server Info')
+    .setDescription(`Hello ${message.author.tag}, you requested to see the latest info on this server. They are found below!`)
+    .addFields(
+      { name: 'Server\'s Name', value: (`${message.guild.name}`)},
+      { name: 'Owner', value: (`${message.guild.owner} (${message.guild.owner.id})`)},
+      { name: 'Server\'s ID', value: (`${message.guild.id}`)},
+      { name: 'Region', value: (`${message.guild.region}`)},
+      { name: 'Member Count', value: (`${message.guild.memberCount}`)},
+      { name: 'Boost Tier', value: (`${message.guild.premiumTier}`)},
+      { name: 'Role Count', value: (`${message.guild.roles.cache.size}`)},
+      { name: 'Emoji Count', value: (`${message.guild.emojis.cache.size}`)},
+      { name: 'Created On', value: (`${message.guild.createdAt}`)}
+    )
+    .setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
+    .setTimestamp()
     .setColor('#2F3136')
-    .setDescription('❌ Command is down for maintenance.')
+    //.setDescription('❌ Command is down for maintenance.')
       message.channel.send({ embeds: [embed] });
     }
   }

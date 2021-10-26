75a4a32365c27266f2740dc74b8fc92be00c845d
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-10-26 19:04:19 -0400

    Update ping.js

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -4,9 +4,13 @@ module.exports = {
     name: 'ping',
     description: 'Server info command',
     execute(message) {
-        
+
         let embed = new MessageEmbed()
-        .setDescription(`🏓Pong! ${Date.now() - message.createdTimestamp}ms`)
+        .setTitle('🏓Pong!')
+        .addFields(
+            { name: 'Client Ping', value: `${Date.now() - message.createdTimestamp}ms.`},
+            { name: 'API Latency', value: `${apiLatency}ms.`},
+        )
         .setColor('#2F3136')
         message.reply({ embeds: [embed] });
 

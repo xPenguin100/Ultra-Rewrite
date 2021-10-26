dcad75f8865fa2eb95bfda8814b89261c87645e1
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-10-25 22:13:46 -0400

    Update user.js

--- commands/information/user.js
+++ commands/information/user.js
@@ -19,6 +19,7 @@ module.exports = {
       .addFields(
         { name: 'Joined Server', value: (`${moment.utc(member.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}`), inline: true},
         { name: 'Joined Discord', value: (`${moment.utc(user.createdAt).format('MMMM Do YYYY, h:mm:ss a')}`), inline: true},
+        { name: 'Roles', value: `${member.roles.cache.map}`},
         { name: 'Badges', value: (`\`${user.flags.toArray()}\``)},
         { name: 'Nickname', value: (`${member.displayName}`)},
         { name: 'Bot', value: `${user.bot}`},

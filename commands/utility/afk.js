cf116b38e0b24774be530829791f1b6d4efdbea7
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-10-27 17:44:40 -0400

    Update afk.js

--- commands/utility/afk.js
+++ commands/utility/afk.js
@@ -10,6 +10,6 @@ module.exports = {
         .setDescription(`You have been set to afk\n**Reason :** ${content}`)
         .setColor("GREEN")
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
-        message.channel.send(embed)                
+        message.channel.send({ embeds: [embed] })                
     }
 }

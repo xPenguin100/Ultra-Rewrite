71cc1c36b17461197577757a3888b4501c410889
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-10-27 19:22:09 -0400

    added warn command

---
+++ commands/moderation/warn.js
@@ -0,0 +1,43 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+
+module.exports = {
+    name: 'warn',
+    description: 'Warn',
+    run: async(client, message, args) => {
+        if(!message.member.permissions.has("KICK_MEMBERS")) return message.reply('You cannot execute this command! You are missing the `KICK_MEMBERS` permission.')
+        
+        let author = message.mentions.users.first() || message.author
+        let user = message.members.mentions.first()
+        if(!user) return message.reply('Please mention a user to warn!')
+
+        if(message.mentions.users.first().bot) return message.reply('You cannot warn bots!')
+        if(message.author.id === user.id) return message.reply('You cannot warn yourself!')
+        if(user.id === message.guild.ownerId) return message.reply("You can't warn the owner of this server.")
+
+        const reason = args.slice(1).join(" ")
+        if(!reason) reason = "No reason specified"
+
+        let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
+        if(warnings === 3) return message.reply(`${user} has already reached their maximum warning capacity.`)
+
+        if(warnings === null) {
+            db.set(`warnings_${message.guild.id}_${user.id}`, 1)
+            const warndm = new MessageEmbed()
+            .setTitle(`Warned in ${message.guild.name}`)
+            .addFields(
+                { name: 'Moderator', value: `${message.author}`},
+                { name: 'Reason', value: `${reason}`},
+            )
+            user.send({ embeds: [warndm] })
+            const warnsuccess = new MessageEmbed()
+            .setTitle('User Warned!')
+            .addFields(
+                { name: 'Moderator', value: `${message.author}`},
+                { name: 'User Warned', value: `${user}`},
+                { name: 'Reason', value: `${reason}`},
+            )
+            await message.reply({ embeds: [warnsuccess] })
+          }
+    }
+}
--- index.js
+++ index.js
@@ -201,6 +201,16 @@ client.on('messageCreate', message => {
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
+if (command === 'warn') {
+  client.commands.get('warn').run(client, message, args);
+  }
+});
+
+client.on('messageCreate', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 if (command === 'report') {
   client.commands.get('report').run(client, message, args);
   }

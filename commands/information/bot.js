423600b15024f314e06e9d086918545d56253f9f
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-10-29 15:01:16 -0400

    Update bot.js

--- commands/information/bot.js
+++ commands/information/bot.js
@@ -26,7 +26,7 @@ module.exports = {
             { name: 'Owner', value: `<@515124684946276362>`, inline: true},
             { name: 'Servers', value: `\`${message.client.guilds.cache.size}\``, inline: true},
             { name: 'Users', value: `\`${message.client.users.cache.size}\``, inline: true},
-            { name: 'Discord.js Version', value: '\`13.2.0\`', inline: true},
+            { name: 'Discord.js Version', value: '\`13.3.0\`', inline: true},
             { name: 'Node Version', value: '\`16.12.0\`', inline: true},
         )
         .setColor('#2F3136')

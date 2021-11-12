c64b038e5bf7d84d41a38fd9cb808fa717b5fdf7
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-11-11 23:08:28 -0500

    Update server.js

--- commands/information/server.js
+++ commands/information/server.js
@@ -12,20 +12,20 @@ module.exports = {
       let avatar = author.displayAvatarURL({size: 4096, dynamic: true});
   
       let embed = new MessageEmbed()
-    .setAuthor(message.guild.name, message.guild.iconURL)
-    .addFields(
-      { name: 'Name', value: (`${message.guild.name} (\`${message.guild.id}\`)`), inline: true},
-      { name: 'Owner', value: `<@${message.guild.ownerId}> (\`${message.guild.ownerId}\`)`},
-      { name: 'Created On', value: (`${moment.utc(message.guild.createdAt).format('MMMM Do YYYY, h:mm:ss a')}`), inline: true},
-      { name: `Boost Count [\`${message.guild.premiumSubscriptionCount}\`]`, value: `\`${message.guild.premiumTier}\``, inline: true},
-      { name: 'Member Count', value: `${message.guild.members.cache.filter(member => !member.user.bot).size} (${message.guild.members.cache.filter(member => member.user.bot).size})`},
-      { name: `Channel Count [${message.guild.channels.cache.size}]`, value: `Categories: \`${channels.filter(channel => channel.type === 'GUILD_CATEGORY').size}\`, Text: \`${channels.filter(channel => channel.type === 'GUILD_TEXT').size}\`, Voice: \`${channels.filter(channel => channel.type === 'GUILD_VOICE').size}\``},
-    )
-    .addField(`Roles [${message.guild.roles.cache.size}]`, message.guild.roles.cache.map((r) => r).join(", "), false)  
-    .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
-    .setTimestamp()
+    //.setAuthor(message.guild.name, message.guild.iconURL)
+   // .addFields(
+     // { name: 'Name', value: (`${message.guild.name} (\`${message.guild.id}\`)`), inline: true},
+     // { name: 'Owner', value: `<@${message.guild.ownerId}> (\`${message.guild.ownerId}\`)`},
+     // { name: 'Created On', value: (`${moment.utc(message.guild.createdAt).format('MMMM Do YYYY, h:mm:ss a')}`), inline: true},
+     // { name: `Boost Count [\`${message.guild.premiumSubscriptionCount}\`]`, value: `\`${message.guild.premiumTier}\``, inline: true},
+      //{ name: 'Member Count', value: `${message.guild.members.cache.filter(member => !member.user.bot).size} (${message.guild.members.cache.filter(member => member.user.bot).size})`},
+     // { name: `Channel Count [${message.guild.channels.cache.size}]`, value: `Categories: \`${channels.filter(channel => channel.type === 'GUILD_CATEGORY').size}\`, Text: \`${channels.filter(channel => channel.type === 'GUILD_TEXT').size}\`, Voice: \`${channels.filter(channel => channel.type === 'GUILD_VOICE').size}\``},
+    //)
+   // .addField(`Roles [${message.guild.roles.cache.size}]`, message.guild.roles.cache.map((r) => r).join(", "), false)  
+    //.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
+   // .setTimestamp()
     .setColor('#2F3136')
-    //.setDescription('❌ Command is down for maintenance.')
+    .setDescription('❌ Command is down for maintenance.')
       message.channel.send({ embeds: [embed] });
     }
   }

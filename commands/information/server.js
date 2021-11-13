e80b259948241e25350e0a86abfc005d9a323a23
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-11-13 16:51:52 -0500

    Update server.js

--- commands/information/server.js
+++ commands/information/server.js
@@ -13,13 +13,14 @@ module.exports = {
   
       let serverembed = new MessageEmbed()
     .setAuthor(message.guild.name, message.guild.iconURL)
+    .setThumbnail(message.guild.icon)
     .addFields(
       { name: 'Name', value: (`${message.guild.name} (\`${message.guild.id}\`)`), inline: true},
       { name: 'Owner', value: `<@${message.guild.ownerId}> (\`${message.guild.ownerId}\`)`},
       { name: 'Created On', value: (`${moment.utc(message.guild.createdAt).format('MMMM Do YYYY, h:mm:ss a')}`), inline: true},
       { name: `Boost Count [\`${message.guild.premiumSubscriptionCount}\`]`, value: `\`${message.guild.premiumTier}\``, inline: true},
-      { name: 'Member Count', value: `${message.guild.members.cache.filter(member => !member.user.bot).size} humans (${message.guild.members.cache.filter(member => member.user.bot).size} bots)`},
-      { name: `Channel Count [${message.guild.channels.cache.size}]`, value: `Categories: \`${channels.filter(channel => channel.type === 'GUILD_CATEGORY').size}\`, Text: \`${channels.filter(channel => channel.type === 'GUILD_TEXT').size}\`, Voice: \`${channels.filter(channel => channel.type === 'GUILD_VOICE').size}\``},
+      { name: 'Member Count', value: `${message.guild.members.cache.filter(member => !member.user.bot).size} humans (${message.guild.members.cache.filter(member => member.user.bot).size} bots)`, inline: true},
+      { name: `Channel Count [${message.guild.channels.cache.size}]`, value: `Categories: \`${channels.filter(channel => channel.type === 'GUILD_CATEGORY').size}\`, Text: \`${channels.filter(channel => channel.type === 'GUILD_TEXT').size}\`, Voice: \`${channels.filter(channel => channel.type === 'GUILD_VOICE').size}\``, inline: true},
     )
     .addField(`Roles [${message.guild.roles.cache.size}]`, message.guild.roles.cache.map((r) => r).join(" "), false) 
     .addField(`Emojis [${message.guild.emojis.cache.size}]`, message.guild.emojis.cache.map((r) => r).join(" "), false) 

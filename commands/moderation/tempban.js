395b0752e02d4865dae429938fd922c0206a7121
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-11-14 12:27:48 -0500

    Update tempban.js

--- commands/moderation/tempban.js
+++ commands/moderation/tempban.js
@@ -5,8 +5,8 @@ module.exports = {
     name: 'tempban',
     description: 'A command to temporarily ban a user',
     run: async(message, args) => {
-        let memberTarget = message.guild.members.cache.get(target.id);
-        if(!member) return message.reply('You did not specify a user to temp-ban!')
+        let memberTarget = message.guild.members.cache.get(memberTarget.id);
+        if(!memberTarget) return message.reply('You did not specify a user to temp-ban!')
 
         let reason = args.slice(1).join(" ")
         if(!reason) reason = 'No reason'
@@ -14,7 +14,7 @@ module.exports = {
         let timeperiod = args[2]
         if(!timeperiod) return message.reply('How long should this user be banned for? \`?tempban <@user> <reason> <time>\`')
 
-        member.ban();
+        memberTarget.ban();
         const banembed = new MessageEmbed()
         .setTitle('🔨 User Temp-Banned!')
         .setDescription(`${memberTarget} has been temp-banned!\n**Reason:** ${reason}\n**Time:** ${timeperiod}\n**Moderator:** ${message.author}`)

91f44abbf77db5ec8a28f4b87d2a19451be8d3f6
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-11-13 22:46:49 -0500

    Update unscramble.js

--- commands/fun/unscramble.js
+++ commands/fun/unscramble.js
@@ -22,7 +22,7 @@ module.exports = {
             if(msg.content.toLowerCase() == word.toLowerCase()) return message.channel.send(`That's correct! Good job!`);
             else return message.channel.send(`That's incorrect. Better luck next time :/`);
         });
-
+        
         collector.on('end', async(collected) => {
             if(collected.size == 0) message.channel.send(`You timed out! Respond quicker next time.`);
         });

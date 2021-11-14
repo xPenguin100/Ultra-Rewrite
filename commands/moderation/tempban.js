8b16de402d78b752a242d1445faf4890f37bf6a6
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-11-14 12:06:06 -0500

    Added tempban command

---
+++ commands/moderation/tempban.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require("discord.js")
+const ms = require('ms')
+
+module.exports = {
+    name: 'tempban',
+    description: 'A command to temporarily ban a user',
+    run: async(message, args) => {
+        let member = message.mentions.users.first()
+        if(!member) return message.reply('You did not specify a user to temp-ban!')
+
+        let reason = args.slice(1).join(" ")
+        if(!reason) reason = 'No reason'
+
+        let timeperiod = args[2]
+        if(!timeperiod) return message.reply('How long should this user be banned for? \`?tempban <@user> <reason> <time>\`')
+
+        await member.ban()
+        const banembed = new MessageEmbed()
+        .setTitle('🔨 User Temp-Banned!')
+        .setDescription(`${member} has been temp-banned!\n**Reason:** ${reason}\n**Time:** ${timeperiod}\n**Moderator:** ${message.author}`)
+        .setColor('#2F3136')
+        message.reply({ embeds: [embed] })
+
+        setTimeout(async () => {
+            await message.guild.members.unban(member)
+            message.channel.send(`${member} has been unbanned!\n`)
+        }, ms(time))
+    }
+}
--- index.js
+++ index.js
@@ -165,6 +165,16 @@ client.on('messageCreate', message => {
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
+if (command === 'tempban') {
+  client.commands.get('tempban').run(message, args);
+  }
+});
+
+client.on('messageCreate', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 if (command === 'fox') {
   client.commands.get('fox').run(client, message, args);
   }

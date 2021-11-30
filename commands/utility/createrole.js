3bb4934b7179c340738234f56daa6e3a6b333472
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-11-30 16:31:20 -0500

    Update createrole.js

--- commands/utility/createrole.js
+++ commands/utility/createrole.js
@@ -5,16 +5,17 @@ module.exports = {
     description: 'Creates a role with the arguments given',
     run: async(message, args) => {
         if(!message.member.permissions.has("MANAGE_ROLES")) return message.reply('You do not have enough permissions to execute this command!')
-        let name = args.slice(1).join(" ")
+        let name = args.join(" ")
         if(!name) return message.reply('Name is not specified! Please give a valid name for this role.')
         let color = args[2]
         if(!color) color = '#99AAB5'
+        console.log(name)
 
         message.guild.roles.create({
             data: {
                 name: name,
                 color: color,
-                permissions: [VIEW_CHANNELS, SEND_MESSAGES, READ_MESSAGE_HISTORY],
+                permissions: ["VIEW_CHANNELS", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"],
                 mentionable: false,
             }
         })

3355714be4276a58c7f5b79bc666be2f900ffab0
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-11-30 22:12:53 -0500

    Update roleinfo.js

--- commands/information/roleinfo.js
+++ commands/information/roleinfo.js
@@ -11,11 +11,11 @@ module.exports = {
       const role = message.mentions.roles.first()
       const member = message.mentions.members.first() || message.member
  
-      let embed = new MessageEmbed()
-    //.setDescription(`🏷 **${role.name}**\n**ID:** ${role.id}\n**Type:** ${role.hexColor}\n**Created On:** ${moment.utc(role.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**❓ Other**\n**Hoisted:** ${role.hoist}\n**Mentionable:** ${role.mentionable}\n**Managed:** ${role.managed}`)
-    //.setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
-    //.setTimestamp()
-    .setDescription('❌ Command Down for Maintenance!')
+      const embed = new MessageEmbed()
+    .setDescription(`🏷 **${role.name}**\n**ID:** ${role.id}\n**Type:** ${role.hexColor}\n**Created On:** ${moment.utc(role.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**❓ Other**\n**Hoisted:** ${role.hoist}\n**Mentionable:** ${role.mentionable}\n**Managed:** ${role.managed}`)
+    .setFooter("Made with Ultra", "https://cdn.discordapp.com/avatars/866014328464605184/670f18d681e14fb695b1c33b07f3a339.jpg")
+    .setTimestamp()
+    //.setDescription('❌ Command Down for Maintenance!')
     .setColor('#2F3136')
       message.channel.send({ embeds: [embed] });
     }

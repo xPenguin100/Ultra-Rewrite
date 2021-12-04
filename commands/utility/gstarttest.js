df49faac0a6f67bc48b7c53f9deb35accda20d78
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-12-03 23:00:28 -0500

    Update gstarttest.js

--- commands/utility/gstarttest.js
+++ commands/utility/gstarttest.js
@@ -37,7 +37,7 @@ module.exports = {
         let n = await gchannel.send({ embeds: [embed] })
         n.react("🎉")
         setTimeout(() => {
-            if(n.reactions.cache.get("tada").count <= 1) {
+            if(n.reactions.cache.get("🎉").count <= 1) {
                 return message.channel.send("Not enough people for me to draw a winner!")
             }
             let winner = n.reactions.cache.get("🎉").users.cache.filter((u) => !u.bot).random();

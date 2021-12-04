a54309ec475a4f12c9015244c8b62adb2e2e22db
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-12-04 15:44:11 -0500

    Update gstarttest.js

--- commands/utility/gstarttest.js
+++ commands/utility/gstarttest.js
@@ -45,7 +45,7 @@ module.exports = {
 
             collector.on("end", collected => {
   if(collected.count <= 1) return message.reply('Not enough people for me to draw a winner!')
-  const winner = n.reactions.cache.get("🎉").users.cache.filter((u) => !u.bot).random();
+  const winner = collected.get("🎉").users.cache.filter((u) => !u.bot).random();
   message.channel.send(`Congratulations ${winner}! You just won the **${prize}**!`);
             })
         }, ms(args[1]));

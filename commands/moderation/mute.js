1cfa1236b093ec19c62456b737dad1ea38762cf3
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-12-05 22:45:19 -0500

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,36 +1,3 @@
-//const { MessageEmbed } = require("discord.js")
-
-//module.exports = {
-   // name: 'mute',
-   // description: 'Mutes a member',
-   // run: async(message, args)  => {
-        //let user = message.mentions.users.first()
-       // if(!user) return message.reply('I cannot mute when you have not mentioned anyone.')
-
-       // let time = args[1]
-        //if(!time) return message.reply('Duration is not specified!')
-
-       // let reason = args.slice(2).join(" ")
-       // if(!reason) reason = "No reason"
-
-        //const muteembed = new MessageEmbed()
-        //.setTitle('🔇 Mute Successful!')
-        //.addFields(
-         //   { name: 'Member Muted:', value: `${user}`},
-          //  { name: 'Duration', value: `${time}`},
-        //    { name: 'Reason:', value: `\`${reason}\``},
-         //   { name: 'Moderator:', value: `${message.author}`}
-       // )
-       // .setColor('#2F3136')
-       // .setTimestamp()
-       // message.guild.permissionOverwrites.edit(user.id, {
-       //     SEND_MESSAGES: false,
-       // })
-
-       // await message.reply(`${user} has been muted for ${time}. The reason is \`${reason}\``)
-   // }
-//}
-
 const { Discord, Message, MessageEmbed }= require('discord.js')
 const ms = require('ms')
 

33ca074ffa2a19921b6d796ee49ebed6654a3388
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-12-14 19:45:10 -0500

    Remade

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,13 +1,17 @@
-const { MessageEmbed } = require("discord.js")
-
+const { MessageEmbed } = require('discord.js')
+ //.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
 module.exports = {
     name: 'say',
     description: 'Say command',
     run: async(client, message, args) => {
+
+        //VARIABLES
+        let say = args.join(" ")
+
         const sayEmbed = new MessageEmbed()
-        .setColor(`2F3136`)
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dyanmic: true }))
-        .setDescription(args.join(" "))
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true}))
+        .setDescription(say)
+        .setColor('#2F3136')
         message.reply({ embeds: [sayEmbed] })
     }
 }

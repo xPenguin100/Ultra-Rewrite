c0ff44e147f8ff3f33bbabbb1299d293ea80584c
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2021-12-26 11:37:55 -0500

    Update approve.js

--- commands/utility/approve.js
+++ commands/utility/approve.js
@@ -1,32 +1,42 @@
-const { MessageEmbed } = require("discord.js");
+const { Discord, MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: 'approve',
     description: 'Approves a suggestion!',
-    run: async(client, message, args) => {
+    async execute(client, message, args) {
+
         if(!message.member.permissions.has("MANAGE_MESSAGES")) return;
         const messageId = args[0];
         const acceptreason = args.slice(1).join(" ");
-        let suggestionChannel = message.guild.channels.cache.get("867106022455246868")
-        //let suggestionChannel = message.guild.channels.cache.find("suggestions")
-        let suggestedEmbed = await suggestionChannel.messages.fetch(messageId);
-
+        const suggestionChannel = message.guild.channels.cache.find(c => c.name === ':bulb:┃suggestions') || message.guild.channels.cache.find(c => c.name === ':bulb:suggestions') || message.guild.channels.cache.find(c => c.name === ':bulb:-suggestions') ||message.guild.channels.cache.find(c => c.name === 'suggestions') || message.guild.channels.cache.find(c => c.name === ':face_with_monocle:-server-suggestions')
+//let channels = message.guild.channels.filter(c => c.type == 'GUILD_TEXT').array();
+      let suggestedEmbed = await suggestionChannel.messages.fetch(messageId);
         if(!messageId) return message.reply("Please specify a valid message ID!")
         if(!acceptreason) return message.reply('Please specify a reason for accepting this suggestion!')
-            
+
             const data = suggestedEmbed.embeds[0];
             const acceptedEmbed = new MessageEmbed()
-            console.log(data)
-            .setAuthor(data.author.username, data.author.IconURL)
-            .setDescription(data.description)
-            .setColor('2F3136')
-            .addField('Status:', 'Accepted')
-            .addField('Reason', acceptreason)
+            .setTitle(data.title)
+            .setAuthor(data.author.name, data.author.displayAvatar_URL)
+            .setThumbnail(data.author.displayAvatar_URL)
+            .setDescription(`${data.description}\n\n **Status: (Accepted)** \n ${acceptreason}`)
+            .setColor('#00FF23')
+            .setFooter("This is an automated message for suggestions • Made by @Mobius", `${message.guild.iconURL({ dynamic: true })}`)
 
-            suggestedEmbed.edit({ embeds: [acceptedEmbed] })
+            suggestedEmbed.edit({ embeds: [acceptedEmbed] }).then(async (msg) =>{
+            msg.react(':thumbsup:');
+            msg.react(':thumbsdown:');
+
+            console.log(data)
 
             const user = await client.users.cache.find((u) => u.tag === data.author.name)
-            user.send(`Your suggestion has been approved in ${message.guild.name}!`);
-    }
+            const approveEmbed = new MessageEmbed()
+            .setTitle("Suggestion Status")
+            .setDescription(`Your suggestion has been **approved** by a moderator in ${message.guild.name}!\n\n **Suggestion Message Link:** ${suggestedEmbed.url}`)
+            .setColor("BLUE")
+            .setThumbnail(suggestedEmbed.guild.iconURL({ dynamic: true }))
+            user.send({ embeds: [approveEmbed] }); 
+    })
 
     }
+}

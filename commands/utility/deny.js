b26e39cf6330e92b7d9aa9de23eac2f23ee99dce
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2022-01-10 19:51:18 -0500

    Fixed finally!

--- commands/utility/deny.js
+++ commands/utility/deny.js
@@ -15,7 +15,7 @@ module.exports = {
         let suggestedEmbed = await suggestionChannel.messages.fetch(messageId);
 */
 
-        const suggestionChannel = message.guild.channels.cache.get("867106022455246868")
+        const suggestionChannel = message.guild.channels.cache.get("867106022455246868"|| "739909677453934602")
 //let channels = message.guild.channels.filter(c => c.type == 'GUILD_TEXT').array();
 console.log(suggestionChannel)
       let suggestedEmbed = await suggestionChannel.messages.fetch(messageId);

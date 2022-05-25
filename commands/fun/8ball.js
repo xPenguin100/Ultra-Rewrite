11ba2d0ee794e0f1ee5cc78bdf8685551c60faa6
xPenguin100 <90059387+xPenguin100@users.noreply.github.com>
2022-05-25 19:25:09 -0400

    Update 8ball.js

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "8ball command",
 
     async run (message, args) {
-        let replies = ["yes.", "try again in 10 seconds.", "Outlook seems good.", "of course.", "Yes, definitely.", "no.", "Better not tell you now.", "Outlook is not so good..", "nah", "never", "Cannot predict now.", "I dont know.", "I dont know *yet*...", "not a chance.", "I think so.", "only for today!", "not for today c:", "sadly yes..", "sadly no..", "maybe!", "ask again.. later.."];
+        let replies = ["yes.", "Try again later", "Try again in 10 seconds.", "Outlook seems good.", "of course.", "Yes, definitely.", "no.", "Better not tell you now.", "Outlook is not so good..", "nah", "never", "Cannot predict now.", "I dont know.", "I dont know *yet*...", "not a chance.", "I think so.", "only for today!", "not for today c:", "sadly yes..", "sadly no..", "maybe!", "ask again.. later.."];
         
         let question = args.slice().join(" ");
         if(!question) message.channel.send('Please state a question.')
